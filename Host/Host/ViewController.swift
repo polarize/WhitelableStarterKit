5ab1feb710dd44020cc3ce3234a50235bddc469b
Issam Bendaas <issam.bendaas@e-therapists.de>
2021-06-13 17:38:07 +0200

    Add host and shared projects

---
+++ Host/Host/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Host
+//
+//  Created by Issam Bendaas on 13.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
